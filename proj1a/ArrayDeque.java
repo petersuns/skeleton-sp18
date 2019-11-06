048394c7d3a1b6e82c33f96c2eaa1cec7b366211
Shuo Sun <petersun116@gmail.com>
2019-11-06 16:07:28 +0100

    part 2

---
+++ proj1a/ArrayDeque.java
@@ -0,0 +1,134 @@
+public class ArrayDeque<T>{
+
+    public T[] items;
+    private int size;
+//    public int l = items.length;
+//    private Node sentinel;
+
+    // creates an empty list
+    public ArrayDeque(){
+        items =(T[]) new Object[2];
+        size=0;
+        int nextFirst=0;
+        int nextLast = size;
+
+    }
+
+    /** Resizes the underlying array to the target capacity. */
+    private void resize(int capacity) {
+        T[] a = (T[]) new Object[capacity];
+        System.arraycopy(items, 0, a, 0, size);
+        items = a;
+    }
+
+    public void addFirst(T item){
+        if (size==items.length){
+            resize(size*2);
+        }
+        T[] new_list = (T[]) new Object[items.length+1];
+        new_list[0] = item;
+        System.arraycopy(items, 0, new_list, 1, size);
+        size +=1;
+        items = new_list;
+    }
+
+    public void addLast(T item){
+        if (size==items.length){
+            resize(size*2);
+        }
+        items[size] =item;
+        size +=1;
+    }
+
+    public boolean isEmpty(){
+        return size==0;
+    }
+    public int size(){
+        return size;
+    }
+    public int len(){
+        return items.length;
+    }
+
+    public void printDeque(){
+        //Node toPrint = sentinel.next;
+        for (int i = 0; i < size; i++){
+            System.out.print(items[i]);
+            System.out.print(" ");
+            //toPrint = toPrint.next;
+        }
+    }
+
+    public T removeFirst(){
+        if(size == 0){
+            return null;
+        }
+        if (size<items.length*0.25){
+            resize(size/2);
+        }
+        T first_item = items[0];
+        T[] new_list = (T[]) new Object[items.length-1];
+        System.arraycopy(items, 1, new_list, 0, size);
+        items = new_list;
+        size -= 1;
+        return first_item;
+    }
+
+    public T removeLast(){
+        if(size == 0){
+            return null;
+        }
+        T last_item = items[size - 1];
+        items[size-1] = null;
+        size -= 1;
+        return last_item;
+    }
+
+    public T get(int index) {
+        if (size == 0 || index > size - 1 || index < 0) {
+            return null;
+        }
+        return items[index];
+    }
+
+
+    public static void main(String[] args) {
+        ArrayDeque<Integer> Dllist = new ArrayDeque<>();
+        System.out.println("size:" + Dllist.size); // expect 1
+        System.out.println("length:" + Dllist.len()); // expect 9
+        Dllist.addLast(1);
+        System.out.println("size:" + Dllist.size); // expect 1
+        System.out.println("length:" + Dllist.len()); // expect 9
+        Dllist.addLast(2);
+        System.out.println("size:" + Dllist.size); // expect 1
+        System.out.println("length:" + Dllist.len()); // expect 9
+        Dllist.addLast(3);
+        System.out.println("size:" + Dllist.size); // expect 1
+        System.out.println("length:" + Dllist.len()); // expect 9
+        Dllist.addLast(4);
+        System.out.println("size:" + Dllist.size); // expect 1
+        System.out.println("length:" + Dllist.len()); // expect 9
+        Dllist.addLast(5);
+        System.out.println("size:" + Dllist.size); // expect 1
+        System.out.println("length:" + Dllist.len()); // expect 9
+        Dllist.printDeque();
+//        System.out.println(); // expect 1
+//        System.out.println("Test get #1");
+//        System.out.println(Dllist.get(0)); // expected 666
+//        System.out.println(Dllist.get(1)); // expected 6666
+//        System.out.println(Dllist.get(2)); // expected 66666
+//        System.out.println(Dllist.get(3)); // expected null
+//        System.out.println(); // expect 1
+//        Dllist.removeFirst();
+//        Dllist.removeLast();
+//        Dllist.printDeque();
+//        System.out.println(); // expect 1
+//        System.out.println("Test get #1");
+//        System.out.println(Dllist.get(0)); // expected 666
+//        System.out.println(Dllist.get(1)); // expected 6666
+//        System.out.println(Dllist.get(2)); // expected 66666
+//        System.out.println(Dllist.get(3)); // expected null
+
+    }
+
+}
